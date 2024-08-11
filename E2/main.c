c021d198a5c2b930257cccab19cecd3de4637c79
Bianca Fernandes Silva <bianca.fernandes@ufba.br>
2024-08-11 09:44:29 -0300

    add my solution

--- E2/main.c
+++ E2/main.c
@@ -1,2 +1,22 @@
 /* main.c */
 
+#include <stdio.h>
+#include "token.h"
+
+extern int yylex();
+extern char *yytext;
+
+int main(int argc, char **argv)
+{
+    token_t token = -1;
+    while (token != EOL){
+        token=yylex();          //chamada do próximo token
+        if (token == ERROR){
+            printf("erro léxico: %s\n", yytext);
+        } else if (token == NUMBER){
+            printf("constante decimal: %s\n", yytext);
+        } else {        
+            printf("Código do token: %d\n", token);
+        }        
+    }
+}
